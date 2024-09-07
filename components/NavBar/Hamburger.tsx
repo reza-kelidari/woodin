276be9b6051a26732e5a77bb4c918976c8116ec2
Reza Kelidari <re.kelidari@gmail.com>
2024-09-07 16:51:47 +0330

    Improving Navbar

--- components/NavBar/Hamburger.tsx
+++ components/NavBar/Hamburger.tsx
@@ -14,8 +14,8 @@ export default function Hamburger() {
 
       <ul
         className={`${
-          !opened && "-translate-y-96"
-        } transition-all translate-y-0 flex flex-col h-fit rounded-2xl justify-between bg-white shadow-md w-fit mx-auto px-16 py-8 items-center gap-4 sm:hidden absolute inset-x-0 top-20 text-xl`}
+          !opened ? "-translate-y-96" : "translate-y-0"
+        } transition-all flex flex-col h-fit rounded-2xl justify-between bg-white dark:bg-zinc-800 shadow-lg w-fit mx-auto px-16 py-8 items-center gap-4 sm:hidden absolute inset-x-0 top-24 text-xl`}
       >
         <li>
           <a href="#" className="hover:text-cyan-700">
--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Hamburger from "./components/Hamburger";
+import Hamburger from "./Hamburger";
 
 export default function NavBar() {
   return (
-    <nav className="flex items-center justify-between bg-slate-100 p-4 fixed w-screen">
-      <h1 className="text-2xl">وودین</h1>
+    <nav className="flex items-center justify-between shadow-lg shadow-slate-100 dark:bg-zinc-900 dark:shadow-zinc-900 p-4 fixed w-screen z-50">
+      <a href="/" className="text-2xl">
+        وودین
+      </a>
       <ul className="flex items-center gap-4 max-sm:hidden">
         <li className="relative dropdown group">
           <a href="#" className="hover:text-cyan-700">
@@ -12,12 +14,16 @@ export default function NavBar() {
           </a>
 
           <div className="dropdown-menu absolute hidden group-hover:block">
-            <ul className="bg-white p-4 shadow-lg min-w-40 h-auto mt-4 flex flex-col gap-4 rounded-lg">
+            <ul className="bg-white dark:bg-zinc-800 p-4 shadow-lg min-w-40 h-auto mt-4 flex flex-col gap-4 rounded-lg">
               <li>
-                <a href="#" className="hover:text-cyan-700">دکوراسیون خانگی</a>
+                <a href="#" className="hover:text-cyan-700">
+                  دکوراسیون خانگی
+                </a>
               </li>
               <li>
-                <a href="#" className="hover:text-cyan-700">دکوراسیون اداری</a>
+                <a href="#" className="hover:text-cyan-700">
+                  دکوراسیون اداری
+                </a>
               </li>
             </ul>
           </div>
